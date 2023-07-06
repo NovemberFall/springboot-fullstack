fb4e0e12c5a36f0a45cd9532a43b096ab0e12df5
TomZheng <1219.zheng@gmail.com>
2023-07-06 13:44:30 -0700

    UPDATE a v0 version of fullstak project with FrontEnd react

--- backend/src/main/java/com/fullstack/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/fullstack/security/SecurityFilterChainConfig.java
@@ -38,8 +38,6 @@ public class SecurityFilterChainConfig {
                 .permitAll()
                 .requestMatchers(HttpMethod.GET, "/ping")
                 .permitAll()
-                .requestMatchers(HttpMethod.GET, "/actuator/**")
-                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
